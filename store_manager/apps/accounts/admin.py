3457c46035a77a4622ab5331d72200812ae8f2e6
João Rafael <150556904+JoaoRafael04@users.noreply.github.com>
2024-09-30 17:26:25 -0300

    Update admin.py

--- store_manager/apps/accounts/admin.py
+++ store_manager/apps/accounts/admin.py
@@ -1,26 +1,25 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser  # Ensure you're importing the correct model
+from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('email', 'full_Name', 'cpf', 'phone_number', 'is_staff', 'is_active')
-    search_fields = ('email', 'full_Name', 'cpf', 'phone_number')
+    model = CustomUser
+    list_display = ('email', 'username', 'full_name', 'cpf', 'phone_number', 'city', 'state', 'country', 'is_staff')
+    search_fields = ('email', 'username', 'cpf', 'full_name')
     readonly_fields = ('date_joined', 'last_login')
-
+    
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('full_Name', 'cpf', 'phone_number')}),
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal Info', {'fields': ('full_name', 'cpf', 'phone_number', 'street', 'home_number', 'city', 'state', 'country')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
-
+    
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'full_Name', 'cpf', 'phone_number', 'password1', 'password2', 'is_staff', 'is_active')}
-        ),
+            'fields': ('email', 'username', 'full_name', 'cpf', 'phone_number', 'password1', 'password2', 'is_staff', 'is_active', 'groups', 'user_permissions'),
+        }),
     )
-
-    ordering = ('email',)
 
 admin.site.register(CustomUser, CustomUserAdmin)
